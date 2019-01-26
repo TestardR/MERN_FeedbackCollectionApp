b9d4f934aa397932d877738319ace3a99d66f038
Romain <rmtestard@gmail.com>
2019-01-26 11:17:52 +0100

    adding nodemon

--- server/index.js
+++ server/index.js
@@ -13,8 +13,10 @@ passport.use(
       callbackURL: '/auth/google/callback',
       userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token : ', accessToken);
+      console.log('refresh token : ', refreshToken);
+      console.log('profile : ', profile);
     }
   )
 );
