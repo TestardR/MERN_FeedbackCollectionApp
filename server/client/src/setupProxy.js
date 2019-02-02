705c5546d1fb82f89ec8df6ce7ea9f227e9bc0de
Romain <rmtestard@gmail.com>
2019-02-02 09:29:06 +0100

    proxy setup for react-app > 2.0

---
+++ server/client/src/setupProxy.js
@@ -0,0 +1,8 @@
+// proxy setup with create-react-app 2.0 >
+
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
+  app.use(proxy('/api/*', { target: 'http://localhost:5000' }));
+};
