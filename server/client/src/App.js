1cc911ee6d6102f5f52bc5f74868ad3994166119
Romain <rmtestard@gmail.com>
2019-02-03 17:30:34 +0100

    react router setup

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
+// BrowserRouter is the brain and tells react how to handle routes
+// Route : rule between a certain route and a component
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <a href="/auth/google">Sign In With Google</a>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Landing} />
+            <Route path="/" component={Header} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
