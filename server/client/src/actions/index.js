58e794dc1648604eda8bbbe39335ecfa102781a6
Romain <rmtestard@gmail.com>
2019-02-04 17:26:59 +0100

    FETCH_USER action with ReduxThunk

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+// Whenever our action is called, it will return an action
+// ReduxThunk will see the function and call it with the "dispatch"
+// We will make a request, and only when we have a response, we sispatch the action with the payload
 const fetchUser = () => {
-  axios.get('/api/current_user');
+  return function(dispatch) {
+    axios
+      .get('/api/current_user')
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
