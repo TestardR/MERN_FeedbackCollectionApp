7b09b8c2cbbe791b1011739a2ca5a37d9da44d4e
Romain <rmtestard@gmail.com>
2019-02-05 09:02:13 +0100

    alternative content in Header according to auth status in paylaod

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
-            Emaily
-          </a>
+          <a className="brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a href="#">Login With Google</a>
-            </li>
+            <ul>{this.renderContent()}</ul>
           </ul>
         </div>
       </nav>
