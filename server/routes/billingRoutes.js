49cb8909878b7f137dfef90094cba0af4ce81d34
unknown <rmtestard@gmail.com>
2019-02-15 15:48:19 +0100

    route specific middleware to check if the user is logged in

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,11 +1,10 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
-    if (!req.user) {
-      return res.status(401).send({ error: 'You must log in' }); // Unauthorized - Forbidden
-    }
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    // the requireLogin middleware checks if the user is logged in
     try {
       const charge = await stripe.charges.create({
         amount: 500,
