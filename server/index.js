a5b8d72155746be9b9b9380c30d371dac9427ed1
unknown <rmtestard@gmail.com>
2019-02-16 17:17:22 +0100

    implementing relationships between Schemas

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 require('./models/User'); // User Schema
+require('./models/Survey');
 require('./services/passport'); // Passport - Google Strategy -
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
