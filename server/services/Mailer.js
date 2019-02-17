7a02484196196a5590c013d9a17852af15ff91c2
unknown <rmtestard@gmail.com>
2019-02-17 16:57:33 +0100

    Mailer constructor

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -8,6 +8,7 @@ class Mailer extends helper.Mail {
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
   }
 }
 
