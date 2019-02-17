64e58395ccf1267d6eadb6ceb668fcf468b8fc6a
unknown <rmtestard@gmail.com>
2019-02-17 17:05:44 +0100

    Boilerplate for sending emails

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -5,16 +5,31 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
+
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
+    // assign the body to the content
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    // add the body we want to use in the mail to the content
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
   }
 
   formatAddresses(recipients) {
     return recipients.map(({ email }) => {
       return new helper.Email(email);
     });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
   }
 }
 
