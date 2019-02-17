c46392015f87ac5f19ec64ae814aa1657ab6e184
unknown <rmtestard@gmail.com>
2019-02-17 16:13:44 +0100

    POST route for surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const SUrvey = mongoose.model('surveys');
+
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      // take all the recipients, make an array on the basis of ',', loop this array to give a new array of emails
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
