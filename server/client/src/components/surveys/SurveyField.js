f1b4245690ba924670ffbfb4bcfa5b5382e2c426
unknown <rmtestard@gmail.com>
2019-02-19 18:20:45 +0100

    surveyField implementation with redux-form

---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SurveyField() {
+  return <input />;
+}
+
+export default SurveyField;
--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 // Field component can be used to show different types of elements that will collect data from our user
 
 class SurveyFrom extends Component {
+  renderField() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="surveyTitle" component="input" />
+          {this.renderField()}
           <button type="submit">Submit</button>
         </form>
       </div>
