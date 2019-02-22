d7e30f465652b5a5520446c1eb514c7c09db34f4
unknown <rmtestard@gmail.com>
2019-02-22 10:58:52 +0100

    posting to surveys !

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -28,6 +28,7 @@ export const handleToken = token => async dispatch => {
 };
 
 // Submit survey
-export const submitSurvey = values => {
-  return { type: 'submit_survey' };
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
