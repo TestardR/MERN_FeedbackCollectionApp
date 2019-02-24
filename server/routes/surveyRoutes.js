90ca5b258ece4241c2e1b4cba609a15d39e4a0e1
unknown <rmtestard@gmail.com>
2019-02-24 11:44:47 +0100

    still facing issues with path-parser

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -3,17 +3,24 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+// const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
+const _ = require('lodash');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   app.get('/api/surveys/thanks', (req, res) => {
-    res.send('Thansk for voting!');
+    res.send('Thanks for voting');
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
-    res.send({});
+    const events = _.map(req.body, event => {
+      const pathname = new URL(event.url).pathname;
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      console.log(p.test(pathname));
+    });
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
